013acaa322ed2dbfb30f28507c0accf1fcc92fae
James Clement <jameseclement@gmail.com>
2019-03-04 10:45:14 -0500

    entry and photo controllers  v1

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,7 +1,47 @@
 class Api::V1::EntriesController < ApplicationController
+  before_action :find_entry, only: [:update, :destroy, :show]
+
+
 
   def index
-    render json: Entry.all 
+    render json: Entry.all
+  end
+
+  def create
+    @entry = Entry.create(entry_params)
+    if @entry.save
+      render json: @entry, status: :accepted
+    else
+      render json: { errors: @entry.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    @entry.update(entry_params)
+    if @entry.save
+      render json: @entry, status: :accepted
+    else
+      render json: { errors: @entry.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def show
+    render json: @entry
+  end
+
+  def destroy
+    @entry.destroy
+  end
+
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :date, :story, :trip_id)
+  end
+
+  def find_entry
+    @entry = Entry.find(params[:id])
   end
 
 end
--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,7 +1,46 @@
 class Api::V1::PhotosController < ApplicationController
+  before_action :find_photo, only: [:update, :destroy, :show]
 
   def index
-    render json: Photo.all 
+    render json: Photo.all
   end
+
+  def create
+    @photo = Photo.create(photo_params)
+    if @photo.save
+      render json: @photo, status: :accepted
+    else
+      render json: { errors: @photo.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    @photo.update(photo_params)
+    if @photo.save
+      render json: @photo, status: :accepted
+    else
+      render json: { errors: @photo.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def show
+    render json: @photo
+  end
+
+  def destroy
+    @photo.destroy
+  end
+
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:caption, :entry_id)
+  end
+
+  def find_photo
+    @photo = Photo.find(params[:id])
+  end
+
 
 end
