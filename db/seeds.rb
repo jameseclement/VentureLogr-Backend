fe23ccc6b16b8e4bbf517b87144237206af0eb3e
James Clement <jameseclement@gmail.com>
2019-03-04 10:00:47 -0500

    add routes for trip entry and photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,17 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Trip.destroy_all
+Entry.destroy_all
+
+indonesia = Trip.create(title: "Indonesia Fall 2016", date: Date.parse("October 1, 2016"), location: "Indonesia", description: '2 Months in Indonesia',
+  photo: 'https://cdn.britannica.com/s:700x450/66/183666-004-EB7D9DA1.jpg')
+
+indonesia_day_1 = Entry.create([{ title: "Day 1" , date: Date.parse("October 1, 2016"), story: 'Left sinagpore today, very excited', trip: indonesia}])
+indonesia_day_2 = Entry.create([{ title: "Day 2" , date: Date.parse("October 2, 2016"), story: 'Only sailing, nothing to speak of', trip: indonesia}])
+
+
+japan = Trip.create(title: "Japan Spring 2016", date: Date.parse("March 1, 2016"), location: "Japan", description: '2 Weeks in Tokyo',
+   photo: 'http://www.globalblue.com/corporate/market-insights/article685045.ece/alternates/LANDSCAPE2_970/Tokyo.jpg')
+
+japan_day_1 = Entry.create([{ title: "Day 1" , date: Date.parse("October 1, 2016"), story: 'Arrived at NRT', trip: japan}])
