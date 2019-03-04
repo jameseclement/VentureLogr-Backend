3e1f67e56a0077b7e6e96a81bcf566136ce89305
James Clement <jameseclement@gmail.com>
2019-03-04 09:03:05 -0500

    setup

---
+++ db/migrate/20190304135356_create_entries.rb
@@ -0,0 +1,12 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.text :story
+      t.date :date
+      t.string :trip_id
+
+      t.timestamps
+    end
+  end
+end
